640e4d1c1be2f2f19dc608ebbd5743b74c5a9eab
mathew.d <matvey.dubrovin@omnichannel.ru>
2023-02-14 17:06:40 +0300

    BubbleSorter

---
+++ BubbleSorter/BubbleSorter/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace BubbleSorter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            BubbleSorter<int> sorter = new BubbleSorter<int>();
+
+            int[] unsortedList = { 89, 15, 12, 45, 1 };
+
+            sorter.Sort(unsortedList, Comparer<int>.Default);
+
+            Console.WriteLine("Finish!");
+        }
+
+        /// <summary>
+        ///     Class that implements bubble sort algorithm.
+        /// </summary>
+        /// <typeparam name="T">Type of array element.</typeparam>
+        public class BubbleSorter<T>
+        {
+            /// <summary>
+            ///     Sorts array using specified comparer,
+            ///     internal, in-place, stable,
+            ///     time complexity: O(n^2),
+            ///     space complexity: O(1),
+            ///     where n - array length.
+            /// </summary>
+            /// <param name="array">Array to sort.</param>
+            /// <param name="comparer">Compares elements.</param>
+            public void Sort(T[] array, IComparer<T> comparer)
+            {
+                for (var i = 0; i < array.Length - 1; i++)
+                {
+                    var wasChanged = false;
+                    for (var j = 0; j < array.Length - i - 1; j++)
+                    {
+                        if (comparer.Compare(array[j], array[j + 1]) > 0)
+                        {
+                            var temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                            wasChanged = true;
+                        }
+                    }
+
+                    if (!wasChanged)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
